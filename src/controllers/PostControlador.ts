a63bc0b2172a402c5cc722d3b342c042ed28b36e
Ludevquest <ludevprogbr19@gmail.com>
2024-08-26 16:07:36 -0300

    feat: implementation of edit posts

--- src/controllers/PostControlador.ts
+++ src/controllers/PostControlador.ts
@@ -55,6 +55,29 @@ export default class PostControlador {
   }
 
   editar(req: Request, res: Response) {
+    const { id } = req.params
+    const { titulo, descricao } = req.body
+
+    if (!titulo || !descricao) {
+      return res.status(400).json({
+        mensagem: 'O título e a descrição da postagem são obrigatórios'
+      })
+    }
+
+    const post = posts.find((e) => {
+      return e.id === id
+    })
+
+    if (!post) {
+      return res.status(404).json({
+        mensagem: 'A postagem não existe'
+      })
+    }
+
+    post.titulo = titulo
+    post.descricao = descricao
+
+    return res.status(204).send()
   }
 
   deletar (req: Request, res: Response) {
