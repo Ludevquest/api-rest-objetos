86a2861bf13d0319878be94e647b65a7b335667c
Ludevquest <ludevprogbr19@gmail.com>
2024-08-26 16:14:17 -0300

    feat: implementation of delete posts

--- src/controllers/PostControlador.ts
+++ src/controllers/PostControlador.ts
@@ -81,6 +81,21 @@ export default class PostControlador {
   }
 
   deletar (req: Request, res: Response) {
+    const { id } = req.params
+
+    const post = posts.findIndex((e) => {
+      return e.id === id
+    })
+
+    if (post === -1) {
+      return res.status(404).json({
+        mensagem: 'A postagem não existe'
+      })
+    }
+
+    posts.splice(post, 1)
+
+    return res.status(204).send()
 }
 
 }
