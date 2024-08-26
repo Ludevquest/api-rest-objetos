ceff23d59c5913601d1865fb9e8bb583ce9649e2
Ludevquest <ludevprogbr19@gmail.com>
2024-08-26 14:22:27 -0300

    feat: implementation of detail author

--- src/controllers/AutorControlador.ts
+++ src/controllers/AutorControlador.ts
@@ -5,4 +5,20 @@ export default class AutorControlador {
   listar(req: Request, res: Response) {
     return res.json(autores)
   }
+
+  detalhar(req: Request, res: Response) {
+    const { id } = req.params
+
+    const autor = autores.find((element) => {
+      return element.id === id
+    })
+
+    if (!autor) {
+      return res.status(404).json({
+        mensagem: 'Autor não encontrado'
+      })
+    }
+
+    return res.json(autor)
+  }
 }
